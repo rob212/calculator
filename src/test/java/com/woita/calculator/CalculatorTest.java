63b41c25cba849e91bdc2710c416fef44447624c
Rob McBryde <robertmcbryde83@gmail.com>
2019-04-09 15:38:36 +0100

    addition of CalculatorController to expose new Calculator sum logic as a service. Also added additional accompanying unit test.

---
+++ src/test/java/com/woita/calculator/CalculatorTest.java
@@ -0,0 +1,13 @@
+package com.woita.calculator;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest {
+    private Calculator calculator = new Calculator();
+
+    @Test
+    public void testSum() {
+        assertEquals(5, calculator.sum(2, 3));
+    }
+}
