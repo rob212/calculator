42dce46c98e47f0f11f50a9e41d8bccdb03f4bfe
Rob McBryde <robertmcbryde83@gmail.com>
2019-06-13 15:44:05 +0100

    addition of checkstyles

--- src/main/java/com/woita/calculator/CalculatorController.java
+++ src/main/java/com/woita/calculator/CalculatorController.java
@@ -1,6 +1,8 @@
 package com.woita.calculator;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +18,9 @@ class CalculatorController {
     private Calculator calculator;
 
     @RequestMapping("/sum")
-    String sum(@RequestParam("a") Integer a, @RequestParam("b") Integer b) {
-        return String.valueOf(calculator.sum(a, b));
+    ResponseEntity<Integer> sum(@RequestParam("a") Integer a, @RequestParam("b") Integer b) {
+        // return String.valueOf(calculator.sum(a, b));
+        int response = calculator.sum(a, b);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
--- src/test/java/com/woita/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/woita/calculator/CalculatorApplicationTests.java
@@ -1,7 +1,10 @@
 package com.woita.calculator;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -12,8 +15,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class CalculatorApplicationTests {
 
+	@Autowired
+	private CalculatorController controller;
+
+
 	@Test
 	public void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
-
 }
+		  
